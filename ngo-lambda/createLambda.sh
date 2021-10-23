275c6401df939ac639fe92272a404e6bde3b84f7
Yuliana Apaza <yuliana.apaza94@gmail.com>
2021-10-23 14:24:15 -0500

    Adapting Lambda to chargeback bureau

--- ngo-lambda/createLambda.sh
+++ ngo-lambda/createLambda.sh
@@ -42,7 +42,7 @@ aws cloudformation package --template-file lambda-api-template.yaml \
     --s3-bucket $BUCKETNAME
 
 echo Deploy the Lambda Cloudformation stack
-export CHAINCODEID=ngo
+export CHAINCODEID=chargeback
 export LAMBDA_STACK_NAME=fabric-lambda-stack
 export VPC_STACK_NAME=$NETWORKNAME-fabric-client-node
 export VPCID=$(aws cloudformation describe-stacks --stack-name $VPC_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='VPCID'].OutputValue" --output text --region $REGION)
