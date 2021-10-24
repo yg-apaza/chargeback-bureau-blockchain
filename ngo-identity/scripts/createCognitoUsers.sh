d015b815b92d7677e0b69c10bd0c27bf9d25c0d6
Yuliana Apaza <yuliana.apaza94@gmail.com>
2021-10-23 22:37:18 -0500

    Create cognito users

--- ngo-identity/scripts/createCognitoUsers.sh
+++ ngo-identity/scripts/createCognitoUsers.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is located at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# or in the "license" file accompanying this file. This file is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-# express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
-
 # Cognito Pool info
 REGION=us-east-1
 COGNITO_APIG_LAMBDA_STACK_NAME=cognito-apig-lambda-stack
@@ -23,22 +10,22 @@ echo Cognito pool id is "$COGNITO_USER_POOL_ID"
 TEMP_PASSWORD=ChangeMe123!
 CUSTOM_ATT_FABRIC_USER=custom:fabricUsername
 
-# Donor user
-DONOR_USERNAME=bobdonor
-DONOR_PASSWORD=Welcome123!
-DONOR_FABRIC_USERNAME=ngoDonor
+# Users
+USER_USERNAME=pandauser
+USER_PASSWORD=Welcome123!
+USER_FABRIC_USERNAME=chargebackUser
 
-MANAGER_USERNAME=alicemanager
+MANAGER_USERNAME=paulamanager
 MANAGER_PASSWORD=Welcome123!
-MANAGER_FABRIC_USERNAME=ngoManager
+MANAGER_FABRIC_USERNAME=chargebackManager
 
 # Create users, set Fabric username attribute, and set password
-# Donor user
-echo Creating donor user
-aws cognito-idp admin-create-user --user-pool-id "$COGNITO_USER_POOL_ID" --username "$DONOR_USERNAME" --temporary-password "$TEMP_PASSWORD" --region "$REGION"
-aws cognito-idp admin-update-user-attributes --user-pool-id "$COGNITO_USER_POOL_ID" --username "$DONOR_USERNAME" --user-attributes Name=$CUSTOM_ATT_FABRIC_USER,Value=$DONOR_FABRIC_USERNAME --region "$REGION"
-aws cognito-idp admin-set-user-password --user-pool-id "$COGNITO_USER_POOL_ID" --username "$DONOR_USERNAME" --password "$DONOR_PASSWORD" --permanent --region "$REGION"
-echo Donor user successfully created
+# Normal user
+echo Creating Normal user
+aws cognito-idp admin-create-user --user-pool-id "$COGNITO_USER_POOL_ID" --username "$USER_USERNAME" --temporary-password "$TEMP_PASSWORD" --region "$REGION"
+aws cognito-idp admin-update-user-attributes --user-pool-id "$COGNITO_USER_POOL_ID" --username "$USER_USERNAME" --user-attributes Name=$CUSTOM_ATT_FABRIC_USER,Value=$USER_FABRIC_USERNAME --region "$REGION"
+aws cognito-idp admin-set-user-password --user-pool-id "$COGNITO_USER_POOL_ID" --username "$USER_USERNAME" --password "$USER_PASSWORD" --permanent --region "$REGION"
+echo Normal user successfully created
 echo
 
 # Manager user
@@ -49,5 +36,4 @@ aws cognito-idp admin-set-user-password --user-pool-id "$COGNITO_USER_POOL_ID" -
 echo Manager user successfully created
 
 # Helper to retrieve newly created users
-# aws cognito-idp admin-get-user --user-pool-id $COGNITO_USER_POOL_ID --username $DONOR_USERNAME --region $REGION
-
+# aws cognito-idp admin-get-user --user-pool-id $COGNITO_USER_POOL_ID --username $USER_USERNAME --region $REGION
