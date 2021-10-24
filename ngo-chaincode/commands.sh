47fde9127af8e16db9763925609f3402f02577bc
Yuliana Apaza <yuliana.apaza94@gmail.com>
2021-10-23 21:19:31 -0500

    Update smart contract: Add query for current user

--- ngo-chaincode/commands.sh
+++ ngo-chaincode/commands.sh
@@ -1,13 +1,14 @@
+# Instantiate chaincode on peer node
 docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
     -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
-    cli peer chaincode install -n chargeback -l node -v v0.1 -p /opt/gopath/src/github.com/chargeback
+    cli peer chaincode install -n chargeback -l node -v v0.2 -p /opt/gopath/src/github.com/chargeback
 
-
+# Instantiate chaincode on channel
 docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
     -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
     cli peer chaincode instantiate -o $ORDERER -C mychannel -n chargeback -v v0.1 -c '{"Args":["init"]}' --cafile /opt/home/managedblockchain-tls-chain.pem --tls
 
-
+# Upgrade chaincode on channel
 docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
     -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" -e "CORE_PEER_ADDRESS=$PEER"  \
-    cli peer chaincode upgrade -o $ORDERER -C mychannel -n chargeback -v v0.1 -c '{"Args":["init"]}' --cafile /opt/home/managedblockchain-tls-chain.pem --tls
+    cli peer chaincode upgrade -o $ORDERER -C mychannel -n chargeback -v v0.2 -c '{"Args":["init"]}' --cafile /opt/home/managedblockchain-tls-chain.pem --tls
--- ngo-chaincode/test-chaincode-aws.sh
+++ ngo-chaincode/test-chaincode-aws.sh
@@ -24,3 +24,9 @@ echo Query history for a specific key
 docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
     -e "CORE_PEER_ADDRESS=$PEER" -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" \
     cli peer chaincode query -C $CHANNEL -n chargeback -c '{"Args":["queryHistoryForKey","{\"key\": \"136772b8c4bc84c09f86d8f936ae107a5fcbfbaa25b15d4a9ee7059dac1b312a-0\"}"]}'
+
+echo Query current user
+
+docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \
+    -e "CORE_PEER_ADDRESS=$PEER" -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" \
+    cli peer chaincode query -C mychannel -n chargeback -c '{"Args":["getClientIdentityInfo"]}'
